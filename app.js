6d92e39529531b8ea35f7391a379696d33c0c61b
Benji Mirschel <cliff.mirschel@gmail.com>
2019-08-15 13:52:38 -0400

    refactored app.js

--- app.js
+++ app.js
@@ -5,19 +5,12 @@ const app = express();
 
 //Middleware stands between a request and response
 app.use(express.json());
-// app.get("/", (req, res) => {
-//   res
-//     .status(200)
-//     //.json will auto create json content format which is done by express
-//     .json({ message: "Hello from the server side!", app: "nodetour" });
-// });
-//Specify the version of the API with v1
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-// Get request
-app.get("/api/v1/tours", (req, res) => {
+
+const getALlTours = (req, res) => {
   //Send back all of the tours when someone hits this route
   res.status(200).json({
     status: "success",
@@ -26,11 +19,9 @@ app.get("/api/v1/tours", (req, res) => {
       tours
     }
   });
-});
-//Use : to create a variable :id
-// Use:id? for optional param
-// Get specific tour
-app.get("/api/v1/tours/:id", (req, res) => {
+};
+
+const getTour = (req, res) => {
   //Send back specific tour when someone hits this route
   const id = req.params.id * 1;
   //Loop through the array, and find the element that has same id as the params
@@ -49,10 +40,24 @@ app.get("/api/v1/tours/:id", (req, res) => {
       tour
     }
   });
-});
-//Post request to add a new tour to our data
+};
 
-app.post("/api/v1/tours", (req, res) => {
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID"
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "<Updated tour here...>"
+    }
+  });
+};
+
+const createTour = (req, res) => {
   //Send data from the client to the server
   //req holds all the data that was requested
   const newId = tours[tours.length - 1].id + 1;
@@ -74,26 +79,9 @@ app.post("/api/v1/tours", (req, res) => {
       });
     }
   );
-});
+};
 
-//Patch data
-
-// app.patch("/api/v1/tours/:id", (req, res) => {
-//   if (req.params.id * 1 > tours.length) {
-//     return res.status(404).json({
-//       status: "fail",
-//       message: "Invalid ID"
-//     });
-//   }
-//   res.status(200).json({
-//     status: "success",
-//     data: {
-//       tour: "<Updated tour here...>"
-//     }
-//   });
-// });
-//Delete resource
-app.delete("/api/v1/tours/:id", (req, res) => {
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: "fail",
@@ -105,8 +93,30 @@ app.delete("/api/v1/tours/:id", (req, res) => {
     status: "success",
     data: null
   });
-});
+};
+// Get request for all tours
+// app.get("/api/v1/tours", getALlTours);
+//Use : to create a variable :id
+// Use:id? for optional param
+// Get specific tour
+// app.get("/api/v1/tours/:id", getTour);
+// //Post request to add a new tour to our data
+// app.post("/api/v1/tours", createTour);
+// //Patch data
+// app.patch("/api/v1/tours/:id", updateTour);
+// //Delete resource
+// app.delete("/api/v1/tours/:id", deleteTour);
 
+app
+  .route("/api/v1/tours")
+  .get(getALlTours)
+  .post(createTour);
+
+app
+  .route("/api/v1/tours/:id")
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 //Start server
 const port = 3000;
 app.listen(port, () => {
