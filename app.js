2cd60e29ac2a344b122280522fbd4ca985b94251
Benji Mirschel <cliff.mirschel@gmail.com>
2019-08-14 20:10:38 -0400

    successful post to add a new tour

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 //Create variable called app
 const app = express();
 
+//Middleware stands between a request and response
+app.use(express.json());
 // app.get("/", (req, res) => {
 //   res
 //     .status(200)
@@ -14,7 +16,7 @@ const app = express();
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-
+// Get request
 app.get("/api/v1/tours", (req, res) => {
   //Send back all of the tours when someone hits this route
   res.status(200).json({
@@ -26,8 +28,30 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
-app.post("/", (req, res) => {
-  res.send("You can post to this endpoint...");
+//Post request to add a new tour to our data
+
+app.post("/api/v1/tours", (req, res) => {
+  //Send data from the client to the server
+  //req holds all the data that was requested
+  const newId = tours[tours.length - 1].id + 1;
+  //Take newTour and push it onto tours array
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  //Use writefile since we are inside of a callback function, we do not want to block the event loop
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      //201 status code stands for created
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 //Start server
 const port = 3000;
