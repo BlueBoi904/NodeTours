87df5c7787b05f156d312046f3eccfaa0ccee3e0
Benji Mirschel <cliff.mirschel@gmail.com>
2019-08-20 11:32:57 -0700

    Finished modeling the tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -80,7 +80,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: "fail",
-      message: "Invalid data sent"
+      message: err
     });
   }
   //Send data from the client to the server
