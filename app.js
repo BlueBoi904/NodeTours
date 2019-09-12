f7b51dc272b95dbc1911efa604fae9fc2c518cd8
Benji Mirschel <cliff.mirschel@gmail.com>
2019-09-11 21:47:52 -0400

    Created dynamic error handling route

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+
 // The request response cycle. Middleware stands between request and response. Everything is middleware in express
 
 //2) Route Handlers
@@ -33,13 +34,26 @@ app.use((req, res, next) => {
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRouter);
 
-// Catch all unhandled routes
+// Catch all unhandled routes at the end of the middleware stack
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find '${req.originalUrl}' on this server.`
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find '${req.originalUrl}' on this server.`
+  // });
+  const err = new Error(`Can't find '${req.originalUrl}' on this server.`);
+  err.status = "fail";
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
-  next();
 });
 
 //4) Start server
