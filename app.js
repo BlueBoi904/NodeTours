cfba0136f6ddd1e547304260894b2ef9ba05f9e4
Benji Mirschel <cliff.mirschel@gmail.com>
2019-09-11 22:00:06 -0400

    Added Error handling class

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+
+const AppError = require("./utils/appError");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 //Create variable called app
@@ -36,14 +38,10 @@ app.use("/api/v1/tours", tourRouter);
 
 // Catch all unhandled routes at the end of the middleware stack
 app.all("*", (req, res, next) => {
-  // res.status(404).json({
-  //   status: "fail",
-  //   message: `Can't find '${req.originalUrl}' on this server.`
-  // });
-  const err = new Error(`Can't find '${req.originalUrl}' on this server.`);
-  err.status = "fail";
-  err.statusCode = 404;
-  next(err);
+  // const err = new Error(`Can't find '${req.originalUrl}' on this server.`);
+  // err.status = "fail";
+  // err.statusCode = 404;
+  next(new AppError(`Can't find '${req.originalUrl}' on this server.`, 404));
 });
 
 app.use((err, req, res, next) => {
