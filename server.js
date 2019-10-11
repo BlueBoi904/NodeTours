d1d694db6839af4e72c736c0fe7db74cebbe7702
cliffmirschel <cmirschel@tdstickets.com>
2019-10-11 14:44:06 -0400

    Finished error handling implementation

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const mongoose = require("mongoose");
 
 const dotenv = require("dotenv");
 
+// Catch uncaught exceptions => "Synchronous code"
+
+process.on("uncaughtException", err => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED EXCEPTION: Shutting down...");
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE.replace(
@@ -14,11 +22,24 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false
   })
-  .then(() => console.log("DB connection successful"));
+  .then(() => console.log("DB connection successful"))
+  .catch(err => {
+    console.log(err);
+  });
 
 const app = require("./app");
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+// Catch unhandled rejections => "promise rejections"
+
+process.on("unhandledRejection", err => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION: Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
 });
