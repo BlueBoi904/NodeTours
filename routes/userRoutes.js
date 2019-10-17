01a12614193efa91ffdc2b501d046e0756a4ad7f
cliffmirschel <cmirschel@tdstickets.com>
2019-10-17 15:16:14 -0400

    added signup route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 const userController = require("./../controllers/userController");
 
+const authController = require("./../controllers/authController");
+
+router.post("/signup", authController.signup);
+
 router
   .route("/")
   .get(userController.getAllUsers)
