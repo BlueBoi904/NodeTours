79af81eafc2abd4cd8cbcfe8579aca97b6af5793
BlueBoi904 <cliff.mirschel@gmail.com>
2019-10-21 15:58:50 -0400

    Adding password encryption/hashing

--- models/userModel.js
+++ models/userModel.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 // const slugify = require("slugify");
 
+const bcrypt = require("bcryptjs");
+
 const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
@@ -24,8 +26,26 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, "Please confirm your password"]
+    required: [true, "Please confirm your password"],
+    validate: {
+      // This only works CREATE and SAVE.
+      validator: function(el) {
+        return el === this.password; // abc === abc => true
+      },
+      message: "Passwords do not match"
+    }
   }
+});
+//Encryption will happen between getting the data and saving it to the database with the pre('save')
+//Hashed and salted passwords with bCrypt
+userSchema.pre("save", async function(next) {
+  // Only run this function if password was actually modified.
+  if (!this.isModified("password")) return next();
+  // Has the password
+  this.password = await bcrypt.hash(this.password, 12);
+  // Delete confirm password field so it will not be persisted in the database.
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
